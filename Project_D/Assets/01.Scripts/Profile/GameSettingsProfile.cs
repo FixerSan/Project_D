d2ecc424d2882d4d26bb9c37da0f3f0fa0b9500d
FixerSan <ggomarain1302@gmail.com>
2023-12-31 01:29:03 +0900

    Add: "Template Project Import"

---
+++ Project_D/Assets/01.Scripts/Profile/GameSettingsProfile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Profile/GameSettingsProfile", fileName = "GameSettingsProfile")]
+public class GameSettingsProfile : ScriptableObject
+{
+    public bool isDebuging;
+}
