b9be2006813208b7d33f201bceb6b18a3e65c164
FixerSan <ggomarain1302@gmail.com>
2024-01-21 23:59:46 +0900

    Update: TestScene

--- Project_D/Assets/01.Scripts/Test/TestController.cs
+++ Project_D/Assets/01.Scripts/Test/TestController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -5,14 +6,5 @@ using UnityEngine;
 
 public class TestController : MonoBehaviour
 {
-    public DialogDatas datas;
 
-    public void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            string json = JsonUtility.ToJson(datas, true);
-            File.WriteAllText(Application.dataPath + "/05.Datas/Data_Dialog.json", json);
-        }
-    }
 }
